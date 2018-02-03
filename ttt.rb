5801d910a504685b0acb75250a1c1ac657f7bccd
durranee <durranee@hotmail.com>
2018-02-03 16:06:27 +0000

    requirement noted and class defined

---
+++ ttt.rb
@@ -0,0 +1,13 @@
+# Requirements
+# initialize :- (to be revised) initialize array
+# print_me_pretty :- A method to print ttt table pretty
+# set set_value (player_no, position)
+# within_limit (input) :- to check if input is within limit (0-9)
+# cell_empty? (index) :- checks if particular cell empty
+# game_full? :- checking if there's board is full
+# get_input (player_num) :- method to get input from user
+# winner? :- A method to check if anyones won
+
+class TickTacToe
+
+end

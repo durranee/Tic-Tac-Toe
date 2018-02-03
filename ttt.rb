62e4f06ee0a9077f21a6c877129191065043898d
durranee <durranee@hotmail.com>
2018-02-03 16:15:44 +0000

    print_board method finished (clears the screen and prints current status of the board

--- ttt.rb
+++ ttt.rb
@@ -1,6 +1,6 @@
 # Requirements
 # initialize :- (to be revised) initialize array
-# print_me_pretty :- A method to print ttt table pretty
+# print_board :- A method to print ttt table pretty
 # set set_value (player_no, position)
 # within_limit (input) :- to check if input is within limit (0-9)
 # cell_empty? (index) :- checks if particular cell empty
@@ -15,5 +15,14 @@ class TickTacToe
     9.times { @board << ' ' }
     @players = { 1 => 'X', 2 => 'O' }
   end
-    
+
+  # method to print the board on the screen
+    def print_board
+      system('clear') # Clears the shell window
+      puts "[#{@board[0]}][#{@board[1]}][#{@board[2]}]"
+      puts "[#{@board[3]}][#{@board[4]}][#{@board[5]}]"
+      puts "[#{@board[6]}][#{@board[7]}][#{@board[8]}]"
+    end
+
+
 end

4a489377cb85132125e1ab2226ef8425dc9843a7
durranee <durranee@hotmail.com>
2018-02-03 16:21:59 +0000

    method set_value and within_limit defined

--- ttt.rb
+++ ttt.rb
@@ -8,6 +8,7 @@
 # get_input (player_num) :- method to get input from user
 # winner? :- A method to check if anyones won
 
+# Main class to run the game
 class TickTacToe
   # initializing board with empty spaces
   def initialize
@@ -16,13 +17,22 @@ class TickTacToe
     @players = { 1 => 'X', 2 => 'O' }
   end
 
-  # method to print the board on the screen
-    def print_board
-      system('clear') # Clears the shell window
-      puts "[#{@board[0]}][#{@board[1]}][#{@board[2]}]"
-      puts "[#{@board[3]}][#{@board[4]}][#{@board[5]}]"
-      puts "[#{@board[6]}][#{@board[7]}][#{@board[8]}]"
+# method to print the board on the screen
+  def print_board
+    system('clear') # Clears the shell window
+    puts "[#{@board[0]}][#{@board[1]}][#{@board[2]}]"
+    puts "[#{@board[3]}][#{@board[4]}][#{@board[5]}]"
+    puts "[#{@board[6]}][#{@board[7]}][#{@board[8]}]"
+  end
+
+# method set_value to insert X or O in the location depending on player num
+    def set_value (player_no, position)
+      @board[position] = @players[player_no]
     end
 
+# returns true if user entered between 1-9 for cell number
+  def within_limit?(input)
+    (input >= 0 && input < 9)
+  end
 
 end
